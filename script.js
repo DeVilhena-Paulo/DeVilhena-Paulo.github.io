824d8dd40bd95c17fc0264700c49d3326cff8b10
de Vilhena <pdevilhe@ic.ac.uk>
2025-11-16 12:05:22 +0000

    Add interests, service, activities, and contact sections.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const address = "180 Queen's Gate, South Kensington, London SW7 2AZ";
+const lst = ['k', 'uu', 'cak', 'lai', 're', 'pmisan', 'ehli', 'vaedep'];
+
+function reveal(xs) {
+    xs = lst.join('').split('');
+    xs[2] = '.';
+    xs[5] = '.';
+    xs[14] = '@';
+    xs[22] = '-';
+    xs[25] = '.';
+    return xs.join('');
+}
+
 function toggleAbstract(btn) {
     // record original text
     if (!btn.dataset.originalText) {
@@ -12,4 +25,13 @@ function toggleAbstract(btn) {
     } else {
         btn.textContent = btn.dataset.originalText;
     }
+}
+
+function copy(btn, text) {
+    navigator.clipboard.writeText(text).then(() => {
+        btn.textContent = 'copied';
+        setTimeout(() => {
+            btn.textContent = 'copy';
+        }, 1000);
+    });
 }
